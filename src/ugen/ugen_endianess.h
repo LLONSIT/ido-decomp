3bcc0d7a268d1a2e9d719edfcec7eac3868e69c7
LLONSIT <lonsitsyt456@gmail.com>
2026-03-24 01:01:53 -0500

    Remove ugen_endianess.h

--- src/ugen/ugen_endianess.h
+++
@@ -1,8 +0,0 @@
-#ifndef UGEN_ENDIANESS_H
-#define UGEN_ENDIANESS_H
-var
-  lsb_first: boolean;
-
-#define UGEN_LITTLE_ENDIAN lsb_first
-#define UGEN_BIG_ENDIAN not lsb_first
-#endif /* UGEN_ENDIANESS_H */
